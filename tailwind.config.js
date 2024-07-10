69b490705a8d4d330cb13949463db09e12144c48
Mh-Monzil <monzil246@gmail.com>
2024-07-11 00:28:37 +0600

    logo addded

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        "primaryBlack": "#050505",
+        "primaryWhite": "#F5F5F5",
+      }
+    },
+  },
+  plugins: [],
+}
+
