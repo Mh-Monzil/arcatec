0cdb38ae9fbf552fa92c35988a33cb11d8148e80
Mh-Monzil <monzil246@gmail.com>
2024-07-12 21:58:56 +0600

    articles shadow added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
         "primaryWhite": "#F5F5F5",
         "yellowGreen": "#C6F906",
         "ash": "#959595",
-      }
+      },
+      boxShadow: {
+        'shadowSmall': '0px 3px 12px 0px rgba(5, 5, 5, 0.04)',
+      },
     },
     keyframes: {
       textRotation: {
