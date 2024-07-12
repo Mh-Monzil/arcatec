fd3316411b8d22e0c4e8744045486744559f55e4
Mh-Monzil <monzil246@gmail.com>
2024-07-12 23:22:05 +0600

    gallary updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       boxShadow: {
         'shadowSmall': '0px 3px 12px 0px rgba(5, 5, 5, 0.04)',
       },
+      backgroundImage: {
+        'gradient-overlay': 'linear-gradient(180deg, rgba(5, 5, 5, 0.80) 0%, rgba(5, 5, 5, 0.00) 100%), linear-gradient(0deg, rgba(5, 5, 5, 0.10) 0%, rgba(5, 5, 5, 0.10) 100%)'
+      },
     },
     keyframes: {
       textRotation: {
